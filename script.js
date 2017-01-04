26c29bbf19056b0e0c8408330f01db6d746c9c5d
drd621 <drdavid621@gmail.com>
2017-01-04 08:54:44 -0600

    Complete script plus minor tweaks

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-// Script
+// Displays a random number between 1 and 6 when button is clicked
 
 $(document).ready(function() {
 	$('#button').click(function() {
-		/*$('#dice').text(Math.floor(Math.random() * 6) + 1);*/
-		alert('TEST');
+		$('#dice').text(Math.floor(Math.random() * 6) + 1);
 	});
 });
